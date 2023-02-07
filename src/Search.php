0359f72d61370da4e840929d9f77d4c1e909a623
Marcone-Santo1 <marconemagalhaes203@gmail.com>
2023-02-07 08:50:16 -0300

    Validando requisções

--- src/Search.php
+++ src/Search.php
@@ -5,21 +5,49 @@ class Search
 {
     private $url = "https://viacep.com.br/ws/";
 
-    public function run ($argc, $argv)
+    public function run (int $argc, array $argv) : void
     {
-        if ($argc != 2) {
-            die("Error, 2 params expected, given $argc params.");
-        }
-        $zipCode = $argv[1];
-        $result = $this->getAdderessFromZipcode($zipCode);
 
+        $zipCode = $argv[1];
+        $this->validation($argc, $zipCode);
+        $result = $this->getAdderessFromZipcode($zipCode);
         print_r($result);
     }
     private function getAdderessFromZipcode(string $zipCode) : array
     {
         $zipCode = preg_replace('/[^0-9]/im', '', $zipCode);
-        $get = file_get_contents($this->url . $zipCode . "/json");
+        return $this->consultaCep($zipCode);
+    }
 
-        return (array) json_decode($get);
+    private function validation(int $argc, string $zipCode) {
+        if ($argc != 2) {
+            die("Error: 2 params expected, given $argc params.");
+        }
+        if (strlen($zipCode) <> 8) {
+            die("Error: The zip code must be a length of 8 characters, given " .strlen($zipCode));
+        }
+    }
+
+    private function consultaCep(string $zipCode) : array
+    {
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => 'http://viacep.com.br/ws/' . $zipCode . '/json',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'GET',
+            CURLOPT_HTTPHEADER => array(
+                'Accept: application/json'
+            ),
+        ));
+
+        $response = curl_exec($curl);
+        curl_close($curl);
+        return (array) json_decode($response);
     }
 }
